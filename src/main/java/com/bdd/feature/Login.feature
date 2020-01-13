1abe80592e1b4a37b10f0b2190deb0bdbb39f5f2
Kishor9552 <kishorsapkal18@gmail.com>
2020-01-13 17:48:35 +0530

    added bdd

---
+++ src/main/java/com/bdd/feature/Login.feature
@@ -0,0 +1,7 @@
+Feature: Login feature
+Scenario: Login feature Test
+
+Given user is on Login Page
+When title is google
+Then  user enters "username" and "password"
+And user clicks on submit button
---
+++ src/main/java/com/bdd/runner/Runner.java
@@ -0,0 +1,23 @@
+package com.bdd.runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "C:\\Users\\Shree\\Desktop\\Alleclipse\\BDD\\src\\main\\java\\com\\bdd\\feature\\Login.feature",
+		//glue={"stepdefinitions"}, //the path of the step definition files
+		//format= {"pretty","html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, //to generate different types of reporting
+		monochrome = true, //display the console output in a proper readable format
+		//strict = false, //it will check if any step is not defined in step definition file
+		dryRun = true //to check the mapping is proper between feature file and step def file
+		//tags = {"~@SmokeTest" , "~@RegressionTest", "~@End2End"}			
+		)
+
+
+
+public class Runner {
+
+}
